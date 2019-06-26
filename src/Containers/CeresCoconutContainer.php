56368197363ada689a3af195caccf672246cbf69
etcshop <mstrauss@etc-shop.de>
2019-06-26 16:43:36 +0200

    VueScripts Einbindung 6

--- src/Containers/CeresCoconutContainer.php
+++ src/Containers/CeresCoconutContainer.php
@@ -9,6 +9,5 @@ class CeresCoconutContainer
     public function call(Twig $twig):string
     {
         return $twig->render('CeresCoconut::Stylesheet');
-        return $twig->render('CeresCoconut::VueScripts');
     }
 }
