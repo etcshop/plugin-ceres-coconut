3b4af7e5b5903a7238ff31ad318db09650acaf36
Michael Strauß <mstrauss@etc-shop.de>
2019-10-06 18:45:08 +0200

    Cookie-State Variable gesetzt 3

--- src/Contexts/CoconutGlobalContext.php
+++ src/Contexts/CoconutGlobalContext.php
@@ -11,6 +11,6 @@ class CoconutGlobalContext extends GlobalContext implements ContextInterface
 
     public function init($params)
     {
-        $this->cookieState = "Nicht bestätigt";
+        $this->cookieState = 1;
     }
 }
