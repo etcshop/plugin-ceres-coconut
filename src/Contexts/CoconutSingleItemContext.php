a696b66aa6d40aac5998d6a1e6333d0bb42106c5
Michael Strauß <33018189+etcshop@users.noreply.github.com>
2020-03-09 11:14:36 +0100

    undo

--- src/Contexts/CoconutSingleItemContext.php
+++ src/Contexts/CoconutSingleItemContext.php
@@ -2,11 +2,7 @@
 
 namespace CeresCoconut\Contexts;
 
-use IO\Helper\Utils;
 use IO\Helper\ContextInterface;
-use IO\Services\CategoryService;
-use IO\Services\CustomerService;
-use Plenty\Plugin\ConfigRepository;
 use Ceres\Contexts\SingleItemContext;
 use Plenty\Modules\Item\Item\Contracts\ItemRepositoryContract;
 
@@ -16,17 +12,13 @@ use IO\Services\ItemSearch\SearchPresets\CrossSellingItems;
 class CoconutSingleItemContext extends SingleItemContext implements ContextInterface
 {
     public $item;
-    public $attributes;
+
     public $variations;
     public $attributeNameMap;
     public $customerShowNetPrices;
-    public $defaultCategory;
-    public $assetName = "ceres-item";
-    public $dynamicVariationId;
-    public $initPleaseSelectOption;
-    public $isItemSet;
     public $ETCItemData;
     public $itemData;
+    public $assetName = "ceres-item";
 
     public $accessory;
     public $similar;
@@ -36,46 +28,9 @@ class CoconutSingleItemContext extends SingleItemContext implements ContextInter
     {
         parent::init($params);
 
-        /** @var CustomerService $customerService */
-        $customerService = pluginApp(CustomerService::class);
-        /** @var ConfigRepository $configRepository */
-        $configRepository = pluginApp(ConfigRepository::class);
-
-        $this->dynamicVariationId = intval($params['dynamic']['documents'][0]['id'] ?? 0);
-        $this->initPleaseSelectOption = $this->getParam('initPleaseSelectOption', false);
-
         $this->item = $params['item'];
         $itemData = $this->item['documents'][0]['data'];
 
-        $availabiltyId = $itemData['variation']['availability']['id'];
-        $mappedAvailability = $configRepository->get('Ceres.availability.mapping.availability' . $availabiltyId);
-        $this->item['documents'][0]['data']['variation']['availability']['mappedAvailability'] = $mappedAvailability;
-
-        $this->isItemSet = $params['isItemSet'];
-
-        $this->attributes = $params['variationAttributeMap']['attributes'];
-        $this->variations = $params['variationAttributeMap']['variations'];
-        $this->customerShowNetPrices = $customerService->showNetPrices();
-
-        $defaultCategoryId = 0;
-        $plentyId = Utils::getPlentyId();
-        foreach ($this->item['documents'][0]['data']['defaultCategories'] as $category) {
-            if ($category['plentyId'] == $plentyId) {
-                $defaultCategoryId = $category['id'];
-                break;
-            }
-        }
-
-        if ($defaultCategoryId > 0) {
-            /** @var CategoryService $categoryService */
-            $categoryService = pluginApp(CategoryService::class);
-            $this->defaultCategory = $categoryService->get($defaultCategoryId);
-        }
-
-        $this->bodyClasses[] = "item-" . $itemData['item']['id'];
-        $this->bodyClasses[] = "variation-" . $itemData['variation']['id'];
-
-//ETC eigene Änderungen
         $itemRep =  pluginApp(ItemRepositoryContract::class);
         $this->ETCItemData = $itemRep->show($itemData['item']['id']);
 
